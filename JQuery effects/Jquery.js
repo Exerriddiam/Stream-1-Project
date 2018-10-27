46d9d23403aba7b4c66ee880de123405db0ad194
matthew addicott <exerriddiam@hotmail.co.uk>
2018-10-27 17:38:31 +0100

    version 4

--- JQuery effects/Jquery.js
+++ JQuery effects/Jquery.js
@@ -1,9 +1,21 @@
 $(document).ready(function() {
 
 	$(window).on("load" ,function() {
-	$(".header1").hide();
-	$("#home").css("sepia","0.9")
-    $(".header1").fadeIn(5000);
+		$(".header1").hide();
+		$("#facebooklink").hide();
+		$("#home").css("sepia","0.9")
+    	$(".header1").fadeIn(2000);
+	});    
+	$(window).on("load" ,function() {
+		$("#facebooklink").fadeIn(5000).delay(20000);
+	});
+	$(window).on("load" ,function() {
+		$(".aboutus1").hide();
+		$(".aboutus1").delay(3000).slideDown(2000).delay(10000).slideUp(2000);
+	});
+	$(window).on("load" ,function() {
+		$(".aboutus2").hide();
+		$(".aboutus2").delay(10000).slideDown(2000).delay(10000).slideUp(2000);
 	});
 
 });
